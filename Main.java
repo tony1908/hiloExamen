6a81ebbadf9afd06cbc0b70dda55050e26cf73b5
Luis Arias <lugearma@gmail.com>
2015-11-02 12:16:22 -0600

    Agregue entrada de usuario

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Productos por dia
+        //int productosDia = 20;
+        int productosDia;
+        productosDia = Teclado.entero("Productos por dia: ");
+
+        //Tamaño de la cola
+        //int tamañoCola = 10;
+        int tamañoCola;
+        tamañoCola = Teclado.entero("Tamaño de la cola: ");
+
+        //Capacidad camiones
+        //int capCamiones = 30;
+        int capCamiones;
+        capCamiones = Teclado.entero("Capacidad de cada camion: ");
+
         //Creamos una cola
-        ColaCircular colaCircular = new ColaCircular(10);
+        ColaCircular colaCircular = new ColaCircular(tamañoCola);
 
         //Creamos a los productoes (deben de ser 4)
         Productor productorUno = new Productor("A", colaCircular);
@@ -11,8 +26,8 @@ public class Main {
         Productor productorCuatro = new Productor("D", colaCircular);
 
         //Creamos a los dos camiones (seran 2)
-        Camiones camionUno = new Camiones("Camion1", colaCircular);
-        Camiones camionDos = new Camiones("Camion2", colaCircular);
+        Camiones camionUno = new Camiones("C1", colaCircular);
+        Camiones camionDos = new Camiones("C2", colaCircular);
 
         productorUno.start();
         productorDos.start();
