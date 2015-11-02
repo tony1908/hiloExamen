7de3e5305f7afddf2571ad325b8f2781157e335c
Luis Arias <lugearma@gmail.com>
2015-11-02 04:05:17 -0600

    Sincronizacion de hilos

--- ColaCircular.java
+++ ColaCircular.java
@@ -18,16 +18,33 @@ public class ColaCircular {
         return !((H == 0 && T == cola.length - 1) || (T + 1 == H));//regresa un "true" si hay una casilla disponible
     }
 
-    public void insertar(String name, int num) {
+    public synchronized void insertar(String nombre, int num) {
+        while (!validaEspacio()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         if (H == -1)
             H = 0;
         else if (T == cola.length - 1)
             T = -1;
         T++;
         cola[T] = num;
+        notifyAll();
+        System.out.println("Se creo el producto: " + num + " por el hilo: " + nombre);
+        listar();
     }
 
-    public int borrar(String nombre) {
+    public synchronized int borrar(String nombre) {
+        while (validaVacio()){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         int aux = cola[H];
         if (H == T)
             H = T = -1;
@@ -35,19 +52,30 @@ public class ColaCircular {
             H = 0;
         else
             H++;
+        notifyAll();
+        System.out.println("El camion " + nombre + " cargo el paquete: " + aux);
+        if(!validaVacio())
+            listar();
         return aux;
     }
 
     public void listar() {
         int i;
-        if (H <= T)
+        if (H <= T){
             for (i = H; i <= T; i++)
-                System.out.println(cola[i]);
+                System.out.print(cola[i] + ", ");
+            System.out.print("\n");
+
+        }
         else {
-            for (i = H; i < cola.length; i++)
-                System.out.println(cola[i]);
-            for (i = 0; i <= T; i++)
-                System.out.println(cola[i]);
+            for (i = H; i < cola.length; i++) {
+                System.out.print(cola[i] + ", ");
+            }
+            System.out.print("\n");
+            for (i = 0; i <= T; i++) {
+                System.out.print(cola[i] + ", ");
+            }
+            System.out.print("\n");
         }
 
     }
--- Main.java
+++ Main.java
@@ -2,19 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         //Creamos una cola
-        ColaCircular colaCircular = new ColaCircular(15);
+        ColaCircular colaCircular = new ColaCircular(10);
 
         //Creamos a los productoes (deben de ser 4)
-        Productor productorUno = new Productor("P1", colaCircular);
+        Productor productorUno = new Productor("A", colaCircular);
+        Productor productorDos = new Productor("B", colaCircular);
+        Productor productorTres = new Productor("C", colaCircular);
+        Productor productorCuatro = new Productor("D", colaCircular);
 
         //Creamos a los dos camiones (seran 2)
-        Camiones camionUno = new Camiones("C1", colaCircular);
-        Camiones camionDos = new Camiones("C2", colaCircular);
+        Camiones camionUno = new Camiones("Camion1", colaCircular);
+        Camiones camionDos = new Camiones("Camion2", colaCircular);
 
         productorUno.start();
+        productorDos.start();
+        productorTres.start();
+        productorCuatro.start();
 
         camionUno.start();
         camionDos.start();
-
     }
 }
